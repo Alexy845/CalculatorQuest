5668b3e41334da2c9c7abe04b9d68d633c3f06ec
Alexy845 <alexy.daubresse@ynov.com>
2023-10-11 14:33:30 +0200

    Ui

---
+++ CalculateurScreen.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CalculatorScreen;
+
+public partial class CalculateurScreen : Window
+{
+    public CalculateurScreen()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CalculatorScreen;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
